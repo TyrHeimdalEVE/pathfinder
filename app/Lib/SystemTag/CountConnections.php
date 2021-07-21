9bcdff2812611c9da5b1313f89b915572a918e00
Tyr Heimdal <tyr.heimdal@warning.no>
2021-07-21 04:23:36 +0200

    Revert tagging temporarily, change NS connection to cyan.

--- app/Lib/SystemTag/CountConnections.php
+++ app/Lib/SystemTag/CountConnections.php
@@ -18,44 +18,17 @@ class CountConnections implements SystemTagInterface
      * @throws \Exception
      */
     static function generateFor(SystemModel $targetSystem, SystemModel $sourceSystem, MapModel $map) : ?string
-    {
-        // set target class for new system being added to the map
-        $targetClass = $targetSystem->security;
-        
-        // Get all systems from active map
-        $systems = $map->getSystemsData();
-                
-        // empty array to append tags to,        
-        // iterate over systems and append tag to $tags if security matches targetSystem security 
-        // and it is not our home (locked)
-        $tags = array();
-        foreach ($systems as $system) {
-          if ($system->security === $targetClass && !$system->locked && $system->tag) {
-            array_push($tags, SystemTag::tagToInt($system->tag));
-          }
-        };
-                
+    {                       
         $whConnections = array_filter($sourceSystem->getConnections(), function (ConnectionModel $connection) {
-          return $connection->isWormhole();
+            return $connection->isWormhole();
         });
         $countWhConnections = count($whConnections);
         if($sourceSystem->locked){
-            if($sourceSystem->Locked && $targetClass == "C5" && !in_array(1, $tags)){
-                $systemTag         = 1;
-                return $systemTag;
-            }
-            if($sourceSystem->Locked && $targetClass == "0.0" && !in_array("SNS", $tags)){
-                $systemTag         = "SNS";
-                return $systemTag;
-            }
-            else {
-                $countWhConnections++;
-        
-                $parentTag          = $sourceSystem->tag;
-                $systemTag          = "$parentTag$countWhConnections";
-                
-                return max($systemTag, 1);
-            }
+            $countWhConnections++;
         }
+        $parentTag          = $sourceSystem->tag;
+        $systemTag          = "$parentTag$countWhConnections";  
+                
+        return max($systemTag, 1);
     }
 }
